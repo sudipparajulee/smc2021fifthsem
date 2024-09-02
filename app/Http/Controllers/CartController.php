2eeb129aa0015695bea6333407fe2336a3dedf04
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-09-02 07:43:45 +0545

    cart validation

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Package;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     public function store(Request $request)
     {
+        $message = [
+            'items.required' => 'Please select at least one item',
+        ];
+        $package = Package::find($request->package_id);
+        $minpeople = $package->capacity * 0.2;
+        $maxpeople = $package->capacity;
         $data = $request->validate([
             'package_id' => 'required',
-            'no_of_people' => 'required',
+            'no_of_people' => "required|numeric|min:$minpeople|max:$maxpeople",
             'items' => 'required',
             'booking_date' => 'required',
-        ]);
+        ],$message);
+
+
     }
 }
--- resources/views/bookpackage.blade.php
+++ resources/views/bookpackage.blade.php
@@ -17,6 +17,9 @@
                         <input type="checkbox" name="selectall" id="selectall" class="mr-2">
                         <label for="selectall">Select All</label>
                     </div>
+                    @error('items')
+                    <p class="text-red-500 ml-2 text-xs">{{$message}}</p>
+                    @enderror
                     @csrf
                     @foreach($items as $item)
                     <div class="flex justify-between p-2 border m-2 rounded hover:bg-gray-100">
@@ -32,9 +35,15 @@
                     <h2 class="text-2xl font-semibold pl-1 ml-2">Checkout</h2>
                     <div class="p-2 m-2">
                         <label for="people" class="ml-2">No of People</label>
-                        <input type="text" name="no_of_people" id="people" class="w-52 block border rounded p-1 ml-2" placeholder="Min {{$package->capacity*0.2}} and Max {{$package->capacity}}">
+                        <input type="text" name="no_of_people" id="people" class="w-52 block border rounded p-1 ml-2" placeholder="Min {{$package->capacity*0.2}} and Max {{$package->capacity}}" value="{{old('no_of_people')}}">
+                        @error('no_of_people')
+                        <p class="text-red-500 ml-2 text-xs">{{$message}}</p>
+                        @enderror
                         <label for="date" class="ml-2">For Date</label>
-                        <input type="date" name="booking_date" id="date" class="w-52 block border rounded p-1 ml-2" min="{{date('Y-m-d')}}">
+                        <input type="date" name="booking_date" id="date" class="w-52 block border rounded p-1 ml-2" min="{{date('Y-m-d')}}" value="{{old('booking_date')}}">
+                        @error('booking_date')
+                        <p class="text-red-500 ml-2 text-xs">{{$message}}</p>
+                        @enderror
                     </div>
                     <button class="bg-blue-600 w-52 text-white px-4 py-2 rounded-lg mt-5 ml-5">Book Now</button>
                     <a href="{{route('viewpackage',$package->id)}}" class="bg-red-600 w-52 text-center text-white px-4 py-2 rounded-lg mt-5 ml-5">Exit</a>
